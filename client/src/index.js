19163387ee5cddc57e979635ff58b07a942312d3
Ashok-Lamichhane <ashok.lamichhane01@gmail.com>
2024-07-11 04:26:46 +0600

    Your commit message

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId="963679257311-ek9ereusnmh5m413mi5dek7ka93fu3ab.apps.googleusercontent.com">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      default: "",
+    },
+    lastName: {
+      type: String,
+      default: "",
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      default: "",
+    },
+    profileImagePath: {
+      type: String,
+      default: "",
+    },
+    tripList: {
+      type: Array,
+      default: [],
+    },
+    wishList: {
+      type: Array,
+      default: [],
+    },
+    propertyList: {
+      type: Array,
+      default: [],
+    },
+    reservationList: {
+      type: Array,
+      default: [],
+    },
+    provider: {
+      type: String,
+      default: "password",
+    },
+  },
+  { timestamps: true }
+)
+
+const User = mongoose.model("User", UserSchema)
+module.exports = User
